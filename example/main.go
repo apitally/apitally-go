49e9afb7c5a639211ff6cbb379bbc548eb165df8
Simon Gurcke <simon@gurcke.de>
2025-04-04 18:15:37 +1100

    WIP

--- example/main.go
+++ example/main.go
@@ -3,17 +3,16 @@ package main
 import (
 	"net/http"
 
-	"github.com/apitally/apitally-go/common"
 	apitally "github.com/apitally/apitally-go/gin"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	config := &common.ApitallyConfig{
+	config := &apitally.ApitallyConfig{
 		ClientId: "54badc91-c693-4db8-9be1-8a281a79dac4",
 		Env:      "dev",
-		RequestLoggingConfig: &common.RequestLoggingConfig{
+		RequestLoggingConfig: &apitally.RequestLoggingConfig{
 			Enabled:            true,
 			LogRequestHeaders:  true,
 			LogResponseHeaders: true,
---
+++ gin/types.go
@@ -0,0 +1,8 @@
+package gin
+
+import (
+	"github.com/apitally/apitally-go/common"
+)
+
+type ApitallyConfig = common.ApitallyConfig
+type RequestLoggingConfig = common.RequestLoggingConfig
